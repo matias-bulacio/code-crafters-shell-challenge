4329f6d99131d2bf01e9a6aae6cff24ad278b327
Dryan Vallik <dryanvallik.gh@yahoo.com>
2025-12-31 14:53:44 -0300

    Add error command not found

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
+#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include "include/z-libs/zstr.h"
+
+zstr get_input() {
+	char buf[1024];
+	fgets(buf, 1024, stdin);
+
+	zstr s = zstr_from(buf);
+	zstr_trim(&s);
+	return s;
+}
+
+void not_found(zstr_view cmd) {
+	zstr_split_iter it = zstr_split_init(cmd, " ");
+	zstr_view name;
+	zstr_split_next(&it, &name);
+
+	fprintf(stdout, "%*s: command not found\n", (int)name.len, name.data);
+}
 
 int main(int argc, char *argv[]) {
     // Flush after every printf
@@ -7,6 +25,8 @@ int main(int argc, char *argv[]) {
 
     // TODO: Uncomment the code below to pass the first stage
     printf("$ ");
+	zstr s = get_input();
+	not_found(zstr_as_view(&s));
 
     return 0;
 }
