3b32d9299ddb7c219e744d1ba92bcaefb3463aa2
Dryan Vallik <dryanvallik.gh@yahoo.com>
2025-12-31 15:21:50 -0300

    Add REPL loop

--- src/main.c
+++ src/main.c
@@ -1,32 +1,43 @@
+#include "../include/z-libs/zstr.h"
+#include <ctype.h>
 #include <stddef.h>
 #include <stdio.h>
-#include "../include/z-libs/zstr.h"
 
 zstr get_input() {
-	char buf[1024];
-	fgets(buf, 1024, stdin);
+    char buf[1024];
+    fgets(buf, 1024, stdin);
 
-	zstr s = zstr_from(buf);
-	zstr_trim(&s);
-	return s;
+    zstr s = zstr_from(buf);
+    zstr_trim(&s);
+    return s;
 }
 
 void not_found(zstr_view cmd) {
-	zstr_split_iter it = zstr_split_init(cmd, " ");
-	zstr_view name;
-	zstr_split_next(&it, &name);
+    size_t first_space = cmd.len;
+    for (int i = 0; i < cmd.len; i++) {
+        if (isspace((int)cmd.data[i])) {
+            first_space = i;
+            break;
+        }
+    }
+    zstr_view name = cmd;
+    name.len = first_space;
+    fprintf(stdout, "%.*s: command not found\n", (int)name.len, name.data);
+}
 
-	fprintf(stdout, "%*s: command not found\n", (int)name.len, name.data);
+void repl() {
+    printf("$ ");
+    zstr cmd = get_input();
+    not_found(zstr_as_view(&cmd));
 }
 
 int main(int argc, char *argv[]) {
     // Flush after every printf
     setbuf(stdout, NULL);
 
-    // TODO: Uncomment the code below to pass the first stage
-    printf("$ ");
-	zstr s = get_input();
-	not_found(zstr_as_view(&s));
+    while (true) {
+        repl();
+    }
 
     return 0;
 }
