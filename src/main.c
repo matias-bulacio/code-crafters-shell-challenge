4c63f0bd8b10e3157aa4114b30b35f1343955661
Dryan Vallik <dryanvallik.gh@yahoo.com>
2026-01-01 17:49:37 -0300

    Implement echo command

--- src/main.c
+++ src/main.c
@@ -12,15 +12,14 @@
 #define REACHED(s)
 #endif
 
-
 zstr get_input() {
     char buf[1024];
     fgets(buf, 1024, stdin);
-	DLN("%lu", strlen(buf));
+    DLN("%lu", strlen(buf));
 
-	REACHED("About to allocate!");
+    REACHED("About to allocate!");
     zstr s = zstr_from(buf);
-	REACHED("Allocated!");
+    REACHED("Allocated!");
     zstr_trim(&s);
     return s;
 }
@@ -29,7 +28,7 @@ void not_found(zstr_view cmd) {
     size_t first_space = cmd.len;
     for (int i = 0; i < cmd.len; i++) {
         if (isspace((int)cmd.data[i])) {
-			REACHED("Is space!");
+            REACHED("Is space!");
             first_space = i;
             break;
         }
@@ -40,19 +39,40 @@ void not_found(zstr_view cmd) {
 }
 
 int run(zstr_view cmd) {
-	if(zstr_view_eq(cmd, "exit")) {
-		exit(EXIT_SUCCESS);
-	}
-	REACHED("Will call not found");
-	not_found(cmd);
-	return 1;
+    if (zstr_view_eq(cmd, "exit")) {
+        exit(EXIT_SUCCESS);
+    }
+    if (zstr_view_starts_with(cmd, "echo")) {
+
+        zstr_split_iter it = zstr_split_init(cmd, " ");
+        zstr_view arg;
+        size_t argc = 0;
+        zstr out = zstr_init();
+        while (argc++, zstr_split_next(&it, &arg)) {
+            if (argc == 1)
+                continue;
+            if (arg.len == 0)
+                continue;
+            if (argc == 2) {
+                zstr_fmt(&out, "%.*s", (int)arg.len, arg.data);
+            } else {
+                zstr_fmt(&out, " %.*s", (int)arg.len, arg.data);
+            }
+        }
+        printf(ZSTR_FMT "\n", ZSTR_ARG(out));
+        zstr_free(&out);
+        return 0;
+    }
+    REACHED("Will call not found");
+    not_found(cmd);
+    return 1;
 }
 
 void repl() {
     printf("$ ");
     zstr cmd = get_input();
     run(zstr_as_view(&cmd));
-	zstr_free(&cmd);
+    zstr_free(&cmd);
 }
 
 int main(int argc, char *argv[]) {
